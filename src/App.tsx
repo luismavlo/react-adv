afed2ce9fb83445604c272de362e9aa0a7a8a647
luismavlo <lmavendano1597@gmail.com>
2022-06-07 10:17:17 -0500

    proyecto inicial listo

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import { Navigation } from './router/Navigation'
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+    </>
+  )
+}
+
+export default App
